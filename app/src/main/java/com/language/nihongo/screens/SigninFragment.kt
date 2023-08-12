0bb6d29c55368748aef97a43662919b54fc200fd
neha_wasnik <neha.wasnik@ftcash.com>
2023-08-13 00:10:40 +0530

    added logo

--- app/src/main/java/com/language/nihongo/screens/SigninFragment.kt
+++ app/src/main/java/com/language/nihongo/screens/SigninFragment.kt
@@ -12,11 +12,7 @@ import com.language.nihongo.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SigninFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class SigninFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
